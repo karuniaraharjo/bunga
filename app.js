e3f96ca514276cee8cf251e789b02d98d7a4d4e6
Runsz <karuniaraharjo06@gmail.com>
2024-04-24 15:17:47 +0700

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("haiii sayangg")
+alert("aku mau kasih kamu hadiah nih")
+alert("liat yaa")
+alert("1")
+alert("2")
+alert("3")
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
